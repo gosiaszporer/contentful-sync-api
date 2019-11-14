269f1264a1c9fd513cbeb55e77dec40c10b9c501
Gosia <gosia.szporer@gmail.com>
2019-11-14 11:55:24 +0100

    Add docs

---
+++ config/initializers/apidoco.rb
@@ -0,0 +1,1 @@
+Apidoco.app_name = 'Offline Agent API'
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'synchronizations/sync' => 'synchronizations#sync'
   get 'synchronizations/reset' => 'synchronizations#reset'
   get 'products/index' => 'products#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount Apidoco::Engine, at: "/docs"
 end
